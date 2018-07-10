9c5d4f6f81112adfbd074a1d11ab22cb9aeff5e4
Nick Grealy <nickgrealy@gmail.com>
2018-07-10 11:36:52 +1000

    workflow implemented.

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var http = require('http');
 var request = require('request-promise');
 
-var STEAM_KEY = process.argv[2]; // i.e. third param e.g. node express.js XXXX
+var STEAM_KEY = process.env.STEAM_API_KEY;
 var LOCAL_SERVER_PORT = 3000;
 
 if (typeof STEAM_KEY === 'undefined' || STEAM_KEY === null || STEAM_KEY.length === 0) {
@@ -42,89 +42,128 @@ API.sendError = function(res) {
 
 var app = express()
 
-app.route('/api/friends/:steamId')
-    .get(function(req, res) {
-        var steamId = req.params.steamId
-        API.getFriends(steamId).then(function(response) {
-            // get a list of all ids...
-            var ids = [steamId]
-            response.friendslist.friends.forEach(function(f) {
-                ids.push(f.steamid);
-            });
+app.route('/')
+    .get((req, res) => {
+        res.type('text/html')
+        res.status(200)
+        res.write('<b>Enter SteamId...</b><hr/>')
+        res.write('<form action="/friends" method="get">')
+        res.write('<label>SteamId:</label>')
+        res.write('<input name="steamid" type="text" autofocus/>')
+        res.write('<input type="submit" value="Next" />')
+        res.write('</form>')
+        res.end()
+    })
+
+app.route('/friends')
+    .get((req, res) => {
+        var mySteamId = req.query.steamid
+        API.getFriends(mySteamId).then(getFriendsRes => {
+            var friendIds = getFriendsRes.friendslist.friends.map(f => f.steamid)
+            friendIds.unshift(mySteamId)
             // get names of friends...
-            var promises = [API.getFriendSummaries(ids)];
-            // get games of friends...
-            ids.forEach(function(id) {
-                promises.push(API.getGames(id));
-            });
-            Promise.all(promises).then(function(responses) {
-                    // map of friends...
-                    var friendsBySteamId = responses[0].response.players.reduce(function(map, p) {
-                        map[p.steamid] = p;
-                        return map;
-                    }, {});
-                    // map of games...
-                    var gamesByAppId = {};
-                    var gameOwnersByAppId = {};
-                    for (var i = 1; i < responses.length; i++) {
-                        var games = responses[i].response.games;
-                        if (typeof games === 'undefined') {
-                            console.error('games was undefined at index ' + i + ' of ' + responses.length, JSON.stringify(responses[i], null, 4));
-                        }
-                        else {
-                            var steamid = ids[i - 1];
-                            games.forEach(function(g) {
-                                gamesByAppId[g.appid] = g;
-                                var owners = gameOwnersByAppId[g.appid];
-                                if (typeof owners === 'undefined') {
-                                    owners = gameOwnersByAppId[g.appid] = [];
-                                }
-                                owners.push(steamid);
-                            });
-                        }
-                    }
-
-                    res.status(200)
-                    res.write('<table border=1 cellspacing=1 cellpadding=4>');
-
-                    // logos
-                    res.write('<tr><th></th><th></th><th>')
-                    res.write(ids.map(function(id) {
-                        return '<img src="' + friendsBySteamId[id].avatarmedium + '"></img>'
-                    }).join('</th><th>'));
-                    res.write('</th></tr>');
-
-                    // names
-                    res.write('<tr><th></th><th>(Game Name)</th><th>')
-                    res.write(ids.map(function(id) {
-                        return '<a href="' + friendsBySteamId[id].profileurl + '">' + friendsBySteamId[id].personaname + '</a>';
-                    }).join('</th><th>'));
-                    res.write('</th></tr>');
-
-                    // games
-                    Object.keys(gamesByAppId).forEach(function(appid) {
-                        var game = gamesByAppId[appid];
-                        var owners = gameOwnersByAppId[appid];
-                        res.write('<tr>')
-                        res.write('<th><img title="' + game.name + '" src="http://media.steampowered.com/steamcommunity/public/images/apps/' + appid + '/' + game.img_logo_url + '.jpg"></img></th>');
-                        res.write('<th>' + game.name + '</th>');
-                        res.write('<th>');
-                        // and whether the user is an owner...
-                        res.write(ids.map(function(id) {
-                            return owners.indexOf(id) !== -1 ? 'TRUE' : '';
-                        }).join('</th><th>'));
-                        res.write('</th></tr>');
-                    });
-                    res.end('</table>');
-
-                },
-                function(error) {
-                    res.status(400).send(error);
+            var promises = []
+            promises.push(API.getFriendSummaries(friendIds))
+            Promise.all(promises).then(responses => {
+                var friends = responses[0].response.players
+                friends = friends.sort((a, b) => a.personaname.localeCompare(b.personaname))
+                res.type('text/html')
+                res.status(200)
+                res.write('<b>Choose friends...</b><hr/>')
+                res.write('<form action="/friends/games" method="get">')
+                res.write('<table border=1 cellspacing=1 cellpadding=4>')
+                res.write('<tr><th>' + 'Select,Avatar,SteamId,Nickname,Realname'.split(',').join('</th><th>') + '</th></tr>')
+                friends.forEach(f => {
+                    res.write('<tr>')
+                    res.write('<td><input type="checkbox" name="friendIds" value="' + f.steamid + '" ' + (f.steamid == mySteamId ? 'checked' : '') + '/>')
+                    res.write('<td>' + ['<img src="' + f.avatarmedium + '"/>', '<a href="' + f.profileurl + '">' + f.steamid + '</a>', f.personaname, f.realname].join('</td><td>') + '</td>')
+                    res.write('</tr>')
                 })
-        }, API.sendError(res));
+                res.write('</table>')
+                res.write('<input type="submit" value="Next" />')
+                res.write('</form>')
+                res.end()
+            })
+        })
+    })
+    
 
-    });
+app.route('/friends/games')
+    .get((req, res) => {
+        var friendIds = req.query.friendIds
+        var promises = []
+        promises.push(API.getFriendSummaries(friendIds))
+        friendIds.forEach(id => promises.push(API.getGames(id)))
+
+        Promise.all(promises).then(responses => {
+            // map of friends...
+            var friendsBySteamId = responses[0].response.players.reduce(function(map, p) {
+                map[p.steamid] = p;
+                return map;
+            }, {});
+            var friends = friendIds.map(id => friendsBySteamId[id])
+            // map of games...
+            var gamesByAppId = {};
+            var gameOwnersByAppId = {};
+            for (var i = 1; i < responses.length; i++) {
+                var games = responses[i].response.games;
+                if (typeof games === 'undefined') {
+                    console.error('games was undefined at index ' + i + ' of ' + responses.length, JSON.stringify(responses[i], null, 4));
+                }
+                else {
+                    var steamid = friendIds[i - 1];
+                    games.forEach(function(g) {
+                        gamesByAppId[g.appid] = g;
+                        var owners = gameOwnersByAppId[g.appid];
+                        if (typeof owners === 'undefined') {
+                            owners = gameOwnersByAppId[g.appid] = [];
+                        }
+                        owners.push(steamid);
+                    });
+                }
+            }
+
+            res.type('text/html')
+            res.status(200)
+            res.write('<b>Result...</b><hr/>')
+            res.write('<table border=1 cellspacing=1 cellpadding=4>');
+
+            // avatars
+            res.write('<tr><th></th><th></th><th>Avatar:</th><th>')
+            res.write(friends.map(f => '<img src="' + f.avatarmedium + '"></img>').join('</th><th>'));
+            res.write('</th></tr>');
+
+            // nicknames
+            res.write('<tr><th></th><th></th><th>Nickname:</th><th>')
+            res.write(friends.map(f => '<a href="' + f.profileurl + '">' + f.personaname + '</a>').join('</th><th>'))
+            res.write('</th></tr>');
+
+            // realnames
+            res.write('<tr><th></th><th></th><th>Realname:</th><th>')
+            res.write(friends.map(f => '<a href="' + f.profileurl + '">' + f.realname + '</a>').join('</th><th>'))
+            res.write('</th></tr>');
+
+            // games
+            Object.keys(gamesByAppId).forEach(function(appid) {
+                var game = gamesByAppId[appid];
+                var owners = gameOwnersByAppId[appid];
+                res.write('<tr>')
+                res.write('<th><img title="' + game.name + '" src="http://media.steampowered.com/steamcommunity/public/images/apps/' + appid + '/' + game.img_logo_url + '.jpg"></img></th>');
+                res.write('<th>' + game.name + '</th>');
+                res.write('<th>' + friendIds.filter(id => owners.indexOf(id) !== -1).length + '</th>');
+                res.write('<th>');
+                // and whether the user is an owner...
+                res.write(friendIds.map(id => owners.indexOf(id) !== -1 ? 'TRUE' : '').join('</th><th>'));
+                res.write('</th></tr>');
+            });
+            res.end('</table>');
+
+        },
+        function(error) {
+            res.status(400).send(error);
+        })
+    })
 
 http.createServer(app).listen(LOCAL_SERVER_PORT);
 
-console.log('Server started - http://127.0.0.1:' + LOCAL_SERVER_PORT + '/api/friends/<YOUR_STEAM_ID>');
+console.log('Server started - http://127.0.0.1:' + LOCAL_SERVER_PORT + '/');
